57bc64856a2c2a8e842301edf92774d377d62e06
mchouza <mchouza@1c9321d8-5a4a-0410-8690-43b0940b6c7b>
2008-04-08 17:32:33 +0000

    - Commit inicial. - Solo contiene la sección de configuración, falta integrarle los módulos y gpcp.

---
+++ afdgp/config_file.cpp
@@ -0,0 +1,124 @@
+//
+// Copyright (c) 2008, Mariano M. Chouza
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright notice, 
+//      this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+//    * The names of the contributors may not be used to endorse or promote
+//      products derived from this software without specific prior written
+//      permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+
+//=============================================================================
+// config_file.cpp
+//-----------------------------------------------------------------------------
+// Creado por Mariano M. Chouza | Creado el 8 de abril de 2008
+//=============================================================================
+
+#include "config_file.h"
+#include <algorithm>
+#include <boost/algorithm/string/trim.hpp>
+#include <boost/regex.hpp>
+#include <fstream>
+
+using namespace Core;
+
+ConfigFile::ConfigFile(const std::string& filename)
+{
+	// Desde un archivo .properties
+	loadFromPropertiesFile(filename);
+}
+
+std::string ConfigFile::readValue(const std::string& key) const
+{
+	TConfigMap::const_iterator it = configMap_.find(key);
+	if (it == configMap_.end())
+		throw; // FIXME: Lanzar algo con cierto sentido
+	return it->second;
+}
+
+std::string ConfigFile::readValue(const std::string& key, 
+								  const std::string& defaultValue) const
+{
+	TConfigMap::const_iterator it = configMap_.find(key);
+	if (it == configMap_.end())
+		return defaultValue;
+	else
+		return it->second;
+}
+
+std::vector<std::string> ConfigFile::getKeys() const
+{
+	using std::string;
+	using std::vector;
+	
+	vector<string> ret;
+
+	TConfigMap::const_iterator it = configMap_.begin();
+	for (; it != configMap_.end(); ++it)
+		ret.push_back(it->first);
+
+	return ret;
+}
+
+void ConfigFile::loadFromPropertiesFile(const std::string& filename)
+{
+	using boost::algorithm::trim;
+	using boost::regex;
+	using boost::regex_match;
+	using boost::smatch;
+	
+	// Trato de abrir el archivo y lanzo una excepción si no está
+	std::ifstream propFile(filename.c_str());
+	if (!propFile.is_open())
+		throw; // FIXME: Lanzar algo más significativo
+
+	// Expresiones regulares para distintos tipos de líneas
+	regex reBlankOrCom("^#.*$");
+	regex reAssignment("^([A-Za-z.-]+)\\s*=\\s*(\\S.*)?");
+	
+	smatch matchRes;
+
+	// Limpio los datos
+	configMap_.clear();
+
+	// Lo leo línea a línea
+	std::string line;
+	while (std::getline(propFile, line))
+	{
+		// Saco espacios al principio y al final
+		trim(line);
+		
+		// Si está en blanco o es un coemntario, la ignoro
+		if (regex_match(line, reBlankOrCom))
+			continue;
+
+		// Uso una expresión regular para partirlo en un par clave-valor
+		if (!regex_match(line, matchRes, reAssignment))
+			// No es ni comentario, ni línea en blanco ni asignación
+			throw; // FIXME: Lanzar algo más significativo
+
+		// Guardo ese par en el mapa (no me preocupo de pisar pares anteriores)
+		configMap_[matchRes.str(1)] = matchRes.str(2);
+	}
+}
---
+++ afdgp/config_file.h
@@ -0,0 +1,73 @@
+//
+// Copyright (c) 2008, Mariano M. Chouza
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright notice, 
+//      this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+//    * The names of the contributors may not be used to endorse or promote
+//      products derived from this software without specific prior written
+//      permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+
+//=============================================================================
+// config_file.h
+//-----------------------------------------------------------------------------
+// Creado por Mariano M. Chouza | Creado el 8 de abril de 2008
+//=============================================================================
+
+#ifndef CONFIG_FILE_H
+#define CONFIG_FILE_H
+
+#include "config.h"
+
+namespace Core
+{
+	/// Configuración a partir de un archivo
+	class ConfigFile : public Config
+	{
+		/// Tipo del mapa con los datos
+		typedef std::map<std::string, std::string> TConfigMap;
+		
+		/// Mapa con los datos
+		TConfigMap configMap_;
+
+		/// Carga el mapa con los datos desde un archivo .properties
+		void loadFromPropertiesFile(const std::string& filename);
+
+	public:
+		/// Constructor a partir de un archivo
+		ConfigFile(const std::string& filename);
+
+		/// Lee un valor dada su clave
+		virtual std::string readValue(const std::string& key) const;
+
+		/// Lee un valor dada su clave con un valor por defecto
+		virtual std::string readValue(const std::string& key, 
+			const std::string& defaultValue) const;
+
+		/// Obtiene las claves
+		virtual std::vector<std::string> getKeys() const;
+	};
+}
+
+#endif
